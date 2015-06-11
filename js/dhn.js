c47dafe9e044c257686ed1d9b7403b6d18a04f74
SimonbJohnson <simonbjohnson@gmail.com>
2015-06-11 16:49:20 +0100

    Compiling auto complete lists improved

--- js/dhn.js
+++ js/dhn.js
@@ -1,3 +1,12 @@
+//spreadsheet columns to search
+var skypeFields = [{name:"description",priority:"low"},
+                    {name:"name",priority:"medium"}];
+var r2wFields = [
+                    {name:"description",priority:"low"},
+                    {name:"name",priority:"low"},
+                    {name:"organisation",priority:"medium"}
+                ];                
+
 // initialise variable to store the spreadsheet data
 
 var R2WData = {};
@@ -6,10 +15,36 @@ var skypeData = {};
 // variable to store terms for searching
 // multiple spreadsheet add to this
 
-var gtags=[];
-var gdescriptions=[];
-var gnames=[];
-var gorgs=[];
+var autoCompleteList = {
+    high:[],
+    medium:[],
+    low:[] 
+};
+
+function compileSearchList(entries,fields){
+    
+    var list = {
+        high:[],
+        medium:[],
+        low:[] 
+    };
+
+    entries.forEach(function(e){
+        var tagslist = e.gsx$tags.$t.split(",");
+        tagslist.forEach(function(e){
+            if(e.substring(0,1)===" "){
+                e=e.substring(1);
+            }
+            list.high.push(e.toLowerCase());
+        });
+        fields.forEach(function(f){
+            list[f.priority].push(e["gsx$"+f.name].$t.toLowerCase());
+        });
+    });
+    autoCompleteList.high = autoCompleteList.high.concat(list.high);
+    autoCompleteList.medium = autoCompleteList.medium.concat(list.medium);
+    autoCompleteList.low = autoCompleteList.low.concat(list.low);  
+}
 
 // remote 2w functionality
 
@@ -20,39 +55,8 @@ var gorgs=[];
 function initR2W(spreadsheetData){
     
     R2WData = spreadsheetData;
-    
-    var resultslist = compileR2WSearchList(spreadsheetData.feed.entry);
-
-    gtags = gtags.concat(resultslist[0]);
-    gdescription = gdescriptions.concat(resultslist[1]);
-    gnames = gnames.concat(resultslist[2]);
-    gorgs = gorgs.concat(resultslist[3]);
-    
-    var list = removeDuplicates(concatLists(gtags,gdescriptions,gnames,gorgs));
-    $( "#searchbox" ).autocomplete({
-      source: list,
-      minLength: 2
-    });    
-}
-
-function compileR2WSearchList(entries){
-    var tags=[];
-    var descriptions=[];
-    var names=[];
-    var orgs=[];
-    entries.forEach(function(e){
-        var tagslist = e.gsx$tags.$t.split(",");
-        tagslist.forEach(function(e){
-            if(e.substring(0,1)===" "){
-                e=e.substring(1);
-            }
-            tags.push(e.toLowerCase());
-        });
-        descriptions.push(e.gsx$description.$t.toLowerCase());
-        names.push(e.gsx$name.$t.toLowerCase());
-        orgs.push(e.gsx$organisation.$t.toLowerCase());
-    });
-    return [tags, descriptions, names, orgs];
+    compileSearchList(R2WData.feed.entry,r2wFields);   
+    updateAutoComplete();
 }
 
 // spreadsheet loaded in parallel so search lists added to overall lists
@@ -112,39 +116,9 @@ function getr2whtml(query,entries){
 
 function initSkypeRooms(spreadSheetData){
 
-    skypeData = spreadSheetData;
-    
-    var resultslist = compileSkypeRoomSearchList(spreadSheetData.feed.entry);
-
-    gtags = gtags.concat(resultslist[0]);
-    gdescription = gdescriptions.concat(resultslist[1]);
-    gnames = gnames.concat(resultslist[2]);
-    
-    var list = removeDuplicates(concatLists(gtags,gdescriptions,gnames,gorgs));
-    $( "#searchbox" ).autocomplete({
-      source: list,
-      minLength: 2
-    });      
-    
-}
-
-function compileSkypeRoomSearchList(entries){
-    var tags=[];
-    var descriptions=[];
-    var names=[];
-    console.log(entries);
-    entries.forEach(function(e){
-        var tagslist = e.gsx$tags.$t.split(",");
-        tagslist.forEach(function(e){
-            if(e.substring(0,1)===" "){
-                e=e.substring(1);
-            }
-            tags.push(e.toLowerCase());
-        });
-        descriptions.push(e.gsx$description.$t.toLowerCase());
-        names.push(e.gsx$name.$t.toLowerCase());
-    });
-    return [tags, descriptions, names];    
+    skypeData = spreadSheetData;    
+    compileSearchList(skypeData.feed.entry,skypeFields);    
+    updateAutoComplete();  
 }
 
 function getSkypeRoomshtml(query,entries){
@@ -188,6 +162,16 @@ function getSkypeRoomshtml(query,entries){
     return html;
 }
 
+// update automcomplete
+
+function updateAutoComplete(){
+    var list = removeDuplicates(concatLists(autoCompleteList.high,autoCompleteList.medium,autoCompleteList.low));
+    $( "#searchbox" ).autocomplete({
+      source: list,
+      minLength: 2
+    });       
+}
+
 //compile search results
 
 function search(query){
@@ -207,8 +191,8 @@ function removeDuplicates(list){
 
 // function to concatenate lists
 
-function concatLists(tags, descriptions, names, orgs){
-    return tags.concat(descriptions.concat(names.concat(orgs)));
+function concatLists(high, medium, low){
+    return high.concat(medium.concat(low));
 }
 
 $('#searchbox').keypress(function (e) {
