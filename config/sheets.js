85763ea7981a1354377fe43cdc6fcbaecc8b8dc3
SJohnson <simonbjohnson@gmail.com>
2015-08-25 17:43:32 +0100

    large code refactor

---
+++ config/sheets.js
@@ -0,0 +1,5 @@
+var sheets = [
+	'https://docs.google.com/spreadsheets/d/1ORRBiQJyEl9UEgWne5VfFCZGpeHcyfe2-JdAEM7f63o/pub?gid=0&single=true&output=csv',
+	'https://docs.google.com/spreadsheets/d/1ORRBiQJyEl9UEgWne5VfFCZGpeHcyfe2-JdAEM7f63o/pub?gid=1609058258&single=true&output=csv',
+	'https://docs.google.com/spreadsheets/d/1ORRBiQJyEl9UEgWne5VfFCZGpeHcyfe2-JdAEM7f63o/pub?gid=1379541339&single=true&output=csv'
+];
--- js/dhn.js
+++ js/dhn.js
@@ -1,202 +1,139 @@
-//spreadsheet columns to search
-var skypeFields = [{name:"description",priority:"low"},
-                    {name:"name",priority:"medium"}];
-var r2wFields = [
-                    {name:"description",priority:"low"},
-                    {name:"name",priority:"low"},
-                    {name:"organisation",priority:"medium"}
-                ];                
-
-// initialise variable to store the spreadsheet data
-
-var R2WData = {};
-var skypeData = {};
-
-// variable to store terms for searching
-// multiple spreadsheet add to this
-
-var autoCompleteList = {
-    high:[],
-    medium:[],
-    low:[] 
-};
-
-function compileSearchList(entries,fields){
-    
-    var list = {
-        high:[],
-        medium:[],
-        low:[] 
-    };
-
-    entries.forEach(function(e){
-        var tagslist = e.gsx$tags.$t.split(",");
-        tagslist.forEach(function(e){
-            if(e.substring(0,1)===" "){
-                e=e.substring(1);
-            }
-            list.high.push(e.toLowerCase());
-        });
-        fields.forEach(function(f){
-            list[f.priority].push(e["gsx$"+f.name].$t.toLowerCase());
-        });
-    });
-    autoCompleteList.high = autoCompleteList.high.concat(list.high);
-    autoCompleteList.medium = autoCompleteList.medium.concat(list.medium);
-    autoCompleteList.low = autoCompleteList.low.concat(list.low);  
-}
-
-// remote 2w functionality
-
-/// creates list of all the search terms
-/// separated into independent lists to prioritise appearance in search box
-//  e.g. tags suggested before names
-
-function initR2W(spreadsheetData){
-    
-    R2WData = spreadsheetData;
-    compileSearchList(R2WData.feed.entry,r2wFields);   
-    updateAutoComplete();
-}
-
-// spreadsheet loaded in parallel so search lists added to overall lists
-// and autocomplete reset.
-
-function getr2whtml(query,entries){
-    var tags=[];
-    var descriptions=[];
-    var names=[];
-    var orgs=[];
-
-    entries.forEach(function(e,index){
-        var tagslist = e.gsx$tags.$t.split(",");
-        tagslist.forEach(function(e){
-            if(e.substring(0,1)===" "){
-                e=e.substring(1);
-            }
-            if(e.toLowerCase()===query){
-                tags.push(index);
-            }
-        });
-        if(query === e.gsx$description.$t.toLowerCase()){
-            descriptions.push(index);
-        }
-        if(query === e.gsx$name.$t.toLowerCase()){
-            names.push(index);
-        }
-        if(query === e.gsx$organisation.$t.toLowerCase()){
-            orgs.push(index);
-        }
-    });
-    
-    var compiledList = tags.concat(descriptions.concat(names).concat(orgs));
-    compiledList = compiledList.filter(function(item, pos, self) {
-        return self.indexOf(item) == pos;
-    });
-    
-    var html ="";
-    if(compiledList.length>0){
-        html = '<div class="sectionTitle"><h2>Remote Who is Doing What</h2></div>';
-        compiledList.forEach(function(e){
-            html += '<div class="resultitem">';
-            if(entries[e].gsx$relevantlink.$t!==""){
-                html +='<h4><a href="'+entries[e].gsx$relevantlink.$t+'" target="_blank">' + entries[e].gsx$description.$t + '</a></h4>';
-            } else {
-                html +="<h4>" + entries[e].gsx$description.$t + "</h4>";
-            }
-            
-            html +="<p>" + entries[e].gsx$organisation.$t + " - " + entries[e].gsx$name.$t +"</p>";
-            html+='</div>';
-        });
+function resultsHTML(result){
+    var html = '<div class="resultitem">';
+    if(result['#meta+url']!=''){
+        html +='<h3 class="title"><a href="'+result['#meta+url']+'" target="_blank">'+result['#meta+title']+'</a></h3><span class="sub">'+result['#meta+category']+'</span>';
+    } else {
+        html +='<h3 class="title">'+result['#meta+title']+'</h3><span class="sub">'+result['#meta+category']+'</span>';
     }
+    html+='<p>'+result['#meta+description']+'</p><p>';
+    if(result['#contact+name']!=''){
+        html+=result['#contact+name']+', ';
+    }
+    html+=result['#org']+'</p></div>';
     return html;
 }
 
-// compiling skype rooms
-
-function initSkypeRooms(spreadSheetData){
-
-    skypeData = spreadSheetData;    
-    compileSearchList(skypeData.feed.entry,skypeFields);    
-    updateAutoComplete();  
-}
-
-function getSkypeRoomshtml(query,entries){
-    var tags=[];
-    var descriptions=[];
-    var names=[];
-
-    entries.forEach(function(e,index){
-        var tagslist = e.gsx$tags.$t.split(",");
-        tagslist.forEach(function(e){
+function searchResults(query){
+    var results = [];
+    dataHXL.forEach(function(d){
+        d['#meta+tags'].split(",").forEach(function(e){
             if(e.substring(0,1)===" "){
                 e=e.substring(1);
             }
-            if(e.toLowerCase()===query){
-                tags.push(index);
-            }
+            if(query.toLowerCase()==e.toLowerCase()){
+                results.push(d);
+            };
         });
-        if(query === e.gsx$description.$t.toLowerCase()){
-            descriptions.push(index);
-        }
-        if(query === e.gsx$name.$t.toLowerCase()){
-            names.push(index);
-        }
+        if(query.toLowerCase()==d['#meta+title'].toLowerCase()){
+            results.push(d);
+        };
+        if(query.toLowerCase()==d['#org'].toLowerCase()){
+            results.push(d);
+        };
+        if(query.toLowerCase()==d['#contact+name'].toLowerCase()){
+            results.push(d);
+        };                        
     });
-    
-    var compiledList = tags.concat(descriptions.concat(names));
-    compiledList = compiledList.filter(function(item, pos, self) {
-        return self.indexOf(item) == pos;
-    });
-    
-    var html ="";
-    if(compiledList.length>0){
-        html = '<div class="sectionTitle"><h2>Skype Rooms</h2></div>';
-        compiledList.forEach(function(e){
-            html += '<div class="resultitem">';
-            html +='<h4>' + entries[e].gsx$name.$t + '</h4>';
-            html +='<p>' + entries[e].gsx$description.$t + '</p>';
-            html+='</div>';
-        });
-    }
-    return html;
-}
-
-// update automcomplete
-
-function updateAutoComplete(){
-    var list = removeDuplicates(concatLists(autoCompleteList.high,autoCompleteList.medium,autoCompleteList.low));
-    $( "#searchbox" ).autocomplete({
-      source: list,
-      minLength: 2
-    });       
+    var html = '<p>Contact the DHN coordinator for more details on anything listed below</p>';
+    results.forEach(function(r){
+        html+=resultsHTML(r);
+    })
+    $('#results').html(html);   
 }
 
 //compile search results
 
 function search(query){
     var html = '<p>Contact the DHN coordinator for more details on anything listed below</p>';
-    html = html + getSkypeRoomshtml(query,skypeData.feed.entry);
-    html = html + getr2whtml(query,R2WData.feed.entry);
+    html = searchResults(query);
     $('#results').html(html);    
+}
+
+function updateAutoComplete(list){
+    console.log(list);
+    $( "#searchbox" ).autocomplete({
+      source: list,
+      minLength: 2,
+      select: function(event, ui) { 
+            search($('#searchbox').val()); }
+    });       
+}
+
+function compileSearchList(data){
+    console.log(data);
+    var searchTerms = [];
+    data.forEach(function(d){
+        d['#meta+tags'].split(",").forEach(function(e){
+            if(e.substring(0,1)===" "){
+                e=e.substring(1);
+            }
+            searchTerms.push(e.toLowerCase());
+        });
+        searchTerms.push(d['#meta+title'].toLowerCase());
+        searchTerms.push(d['#org'].toLowerCase());
+        searchTerms.push(d['#contact+name'].toLowerCase());
+    });
+    return removeDuplicatesSort(searchTerms);
+}
+
+function hxlProxyToJSON(input){
+    var output = [];
+    var keys=[]
+    input.forEach(function(e,i){
+        if(i==0){
+            keys = e;
+        } else {
+            var row = {};
+            e.forEach(function(e2,i2){
+                row[keys[i2]] = e2;
+            });
+            output.push(row);
+        }
+    });
+    return output;
+}
+
+//function to construct hxlprxy URL
+
+function constructHXLURL(linkList){
+    var url = 'http://beta.proxy.hxlstandard.org/data.json?filter_count=7&url=';
+    linkList.forEach(function(l,i){
+        if(i==0){
+            url+=l+'&strip-headers=on&format=html';
+        }
+        if(i==1){
+            url+='&filter01=append&append-dataset01-01='+l;
+        }
+        if(i>1){
+            url+='&append-dataset01-0'+i+'='+l;
+        }
+    });
+    return url//+'&force=1';
 }
 
 //function to remove duplicated from seacrch term lists
 
-function removeDuplicates(list){
+function removeDuplicatesSort(list){
     return list.filter(function(item, pos, self) {
         return self.indexOf(item) === pos;
-    });    
+    }).sort();    
 }
 
-// function to concatenate lists
-
-function concatLists(high, medium, low){
-    return high.concat(medium.concat(low));
-}
+var dataHXL;
 
 $('#searchbox').keypress(function (e) {
     if (e.which === 13) {
         search($('#searchbox').val());
     }
 });
+
+$.ajax(constructHXLURL(sheets), {
+        success: function(data) {
+            dataHXL = hxlProxyToJSON(data);
+            updateAutoComplete(compileSearchList(dataHXL));
+        },
+        error: function(e,err) {
+            console.log(err);
+        }
+});
+
